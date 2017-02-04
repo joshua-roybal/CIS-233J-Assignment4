10cfb4ae0e18100407dae2aa11866a729c88c643
Josh Roybal <Joshua.Roybal@PCC.edu>
2017-02-03 19:05:38 -0800

    added main method that prints a few lines of code.

--- src/assignment4/class1.java
+++ src/assignment4/class1.java
@@ -1,7 +1,14 @@
 package assignment4;
 
 /**
- * Created by Josh on 2/3/17.
+ * Created by Josh Roybal on 2/3/17.
  */
 public class class1 {
+
+    public static void main(String[] args) {
+        System.out.println("Josh Roybal");
+        System.out.println("CIS 233J");
+        System.out.println("Assignment 4");
+    }
+
 }
